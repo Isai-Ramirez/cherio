29311cb7b379b871ae7de0b55c35edf8f6008a1e
isai <Isai4827@4827@gmail.com>
2019-03-14 09:54:38 -0700

    ...

--- server.js
+++ server.js
@@ -79,6 +79,6 @@ app.post("/articles/:id", function (req, res) {
         });
 });
 
-app.listen(PORT, function () {
-    console.log("App running on port " + PORT + "!");
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+   });
