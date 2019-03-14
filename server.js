0a238d55c4cba05832bbc7cd1136107c5ab33aff
isai <Isai4827@4827@gmail.com>
2019-03-14 12:04:15 -0700

    ...

--- server.js
+++ server.js
@@ -1,85 +1,93 @@
 var express = require("express");
+var logger = require("morgan");
 var mongoose = require("mongoose");
-const exphbs = require("express-handlebars");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+// Require all models
 var db = require("./models");
 
 var PORT = 3000;
 
 // Initialize Express
 var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Make public a static folder
 app.use(express.static("public"));
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
-// Routes
-app.get("/", function(req, res){
-    res.render("index")
-})
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/scraperhomework", { useNewUrlParser: true });
+
+
 app.get("/scrape", function (req, res) {
-    axios.get("https://www.reddit.com/r/news").then(function (response) {
-        var $ = cheerio.load(response.data);
-        $("article h2").each(function (i, element) {
-            var result = {};
-            result.title = $(this)
-                .children("a")
-                .text();
-            result.link = $(this)
-                .children("a")
-                .attr("href");
-            // Create a new article using `result` 
-            db.Article.create(result)
-                .then(function (dbArticle) {
-                    console.log(dbArticle);
-                })
-                .catch(function (err) {
-                    console.log(err);
-                });
+  axios.get("https://www.reddit.com/r/news").then(function (response) {
+    var $ = cheerio.load(response.data);
+
+    $("article h2").each(function (i, element) {
+      var result = {};
+
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          console.log(err);
         });
-        res.send("Scrape Complete");
+    });
+
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/articles", function (req, res) {
+  db.Article.find({}, function (error, found) {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(found);
+    }
+  })
+});
+
+app.get("/articles/:id", function (req, res) {
+  db.Article.findOne({ "_id": req.params.id })
+    .populate("note")
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
     });
 });
-//getting all Articles from the db
-app.get("/articles", function (req, res) {
-    db.Article.find({})
-        .then(function (dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function (err) {
-            res.json(err);
-        });
-});
-// Route for grabbing article by id
-app.get("/articles/:id", function (req, res) {
-    db.Article.findOne({ _id: req.params.id })
-        .populate("note")
-        .then(function (dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function (err) {
-            res.json(err);
-        });
-});
 
-// creating and adding the note
 app.post("/articles/:id", function (req, res) {
-    // creating a new note
-    db.Note.create(req.body)
-        .then(function (dbNote) {
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-        })
-        .then(function (dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function (err) {
-            res.json(err);
-        });
+
+  db.Note.create(req.body).then(function(dbNote){
+    return db.Article.findOneAndUpdate({ "_id": req.params.id }, { note: dbNote._id } , { new: true })
+  }) .then(function(dbArticle){
+    res.json(dbArticle)
+  })
+  .catch(function(err){
+    res.json(err);
+  })
 });
 
 app.listen(process.env.PORT || 3000, function(){
